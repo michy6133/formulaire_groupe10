00941499fe41a601fab50ea777ae5adbdde08539
OKOUDJOU Michael Charles <michy6133@gmail.com>
2024-04-29 00:49:50 +0100

    first commit

--- lib/page/form_page.dart
+++ lib/page/form_page.dart
@@ -18,6 +18,7 @@ class _FormPageState extends State<FormPage> {
   late TextEditingController _dateNaissanceController;
   late File _photoPath; // Utilisation de File
   final ImagePicker _picker = ImagePicker();
+  final _formKey = GlobalKey<FormState>(); // Clé pour le formulaire
 
   @override
   void initState() {
@@ -62,22 +63,8 @@ class _FormPageState extends State<FormPage> {
     }
   }
 
-  String? _validateNotEmpty(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Ce champ est requis';
-    }
-    return null;
-  }
-
   void _submitForm() {
-    if (_validateNotEmpty(_nomController.text) != null ||
-        _validateNotEmpty(_prenomController.text) != null ||
-        _validateNotEmpty(_ageController.text) != null) {
-      // Vérifie si les champs nom, prénom et âge sont vides
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Veuillez remplir tous les champs requis')),
-      );
-    } else {
+    if (_formKey.currentState!.validate()) { // Vérification de la validité du formulaire
       PersonData person = PersonData(
         nom: _nomController.text,
         prenom: _prenomController.text,
@@ -100,49 +87,67 @@ class _FormPageState extends State<FormPage> {
       appBar: AppBar(title: Text('Formulaire')),
       body: Padding(
         padding: EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextFormField(
-                controller: _nomController,
-                decoration: InputDecoration(labelText: 'Nom'),
-                validator: _validateNotEmpty,
-              ),
-              TextFormField(
-                controller: _prenomController,
-                decoration: InputDecoration(labelText: 'Prénom'),
-                validator: _validateNotEmpty,
-              ),
-              TextFormField(
-                controller: _ageController,
-                decoration: InputDecoration(labelText: 'Âge'),
-                keyboardType: TextInputType.number,
-                validator: _validateNotEmpty,
-              ),
-              TextFormField(
-                controller: _sexeController,
-                decoration: InputDecoration(labelText: 'Sexe'),
-              ),
-              GestureDetector(
-                onTap: () => _pickDate(context),
-                child: AbsorbPointer(
-                  child: TextFormField(
-                    controller: _dateNaissanceController,
-                    decoration: InputDecoration(labelText: 'Date de naissance'),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(
+                  controller: _nomController,
+                  decoration: InputDecoration(labelText: 'Nom'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Veuillez remplir le champ';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _prenomController,
+                  decoration: InputDecoration(labelText: 'Prénom'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Veuillez remplir le champ';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _ageController,
+                  decoration: InputDecoration(labelText: 'Âge'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Veuillez remplir le champ';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _sexeController,
+                  decoration: InputDecoration(labelText: 'Sexe'),
+                ),
+                GestureDetector(
+                  onTap: () => _pickDate(context),
+                  child: AbsorbPointer(
+                    child: TextFormField(
+                      controller: _dateNaissanceController,
+                      decoration: InputDecoration(labelText: 'Date de naissance'),
+                    ),
                   ),
                 ),
-              ),
-              ElevatedButton(
-                onPressed: _chooseImage,
-                child: Text('Choisir une photo'),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _submitForm,
-                child: Text('Valider'),
-              ),
-            ],
+                ElevatedButton(
+                  onPressed: _chooseImage,
+                  child: Text('Choisir une photo'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _submitForm,
+                  child: Text('Valider'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
